a6f3a81e7836a8bcc1da8fb0a04af62a318b07ff
sam43 <sayemruet.103043@gmail.com>
2017-03-22 15:42:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sam43/kehermaanrndproject/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.sam43.kehermaanrndproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
